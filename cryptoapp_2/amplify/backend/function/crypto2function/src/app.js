265043480e3a20ce907893078212efd1bf30e245
jegyes <jegyes@madisoncollege.edu>
2025-12-30 02:48:54 -0600

    api updated to fetch coinlore.com data

--- cryptoapp_2/amplify/backend/function/crypto2function/src/app.js
+++ cryptoapp_2/amplify/backend/function/crypto2function/src/app.js
@@ -24,17 +24,38 @@ app.use(function(req, res, next) {
 
 /* amplify/backend/function/cryptofunction/src/app.js */
 
-app.get('/coins', function(req, res) {
-  const coins = [
-    { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
-    { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
-    { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
-  ]
-  res.json({
-    coins
-  })
-})
+// app.get('/coins', function(req, res) {
+//   const coins = [
+//     { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
+//     { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
+//     { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
+//   ]
+//   res.json({
+//     coins
+//   })
+// })
 
+
+app.get('/coins', async function(req, res) {
+  // Define base url
+  let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`
+
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+   const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters
+   apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`
+  }
+
+  // Call API and return response
+try {
+  const response = await fetch(apiUrl);
+  const json = await response.json();
+  res.json({coins: json.data});
+} catch(error) {
+  res.json({error: error});
+}
+})
 
 
 /**********************
