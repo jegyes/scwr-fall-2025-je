c09b4a0536f310f440a32534fda1eb178aa3481a
jegyes <jegyes@madisoncollege.edu>
2025-12-30 03:16:24 -0600

    updated app.jsx to receive limit and start parameter input

--- cryptoapp_2/src/App.jsx
+++ cryptoapp_2/src/App.jsx
@@ -7,14 +7,23 @@ import { get } from 'aws-amplify/api'
 import './App.css';
 
 function App() {
-  // Create coins variable and set to empty array
-  const [coins, updateCoins] = useState([])
+// Create coins variable and set to empty array
+const [coins, updateCoins] = useState([])
+
+// Create additional state to hold user input for limit and start properties
+const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+// Create a new function to allow users to update the input values
+function updateInputValues(type, value) {
+  updateInput({ ...input, [type]: value })
+}
 
   // Define function to all API
   async function fetchCoins() {
+    const { limit, start } = input
     const request = get({
       apiName: 'crypto2api',
-      path: '/coins'
+      path: `/coins?limit=${limit}&start=${start}`
     })
     const response = await request.response;
     const data = await response.body.json();
@@ -29,6 +38,16 @@ function App() {
 
   return (
     <div className="App">
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+      {/*Add button to the UI to give user the option to call the API*/} 
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
